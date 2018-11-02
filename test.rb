900c29acb2e69d263004fbad0284f105c8a9b2ff
Lucas Lawrence <lucaslawrencelim@gmail.com>
2018-11-02 16:42:03 +0800

    Random

--- test.rb
+++ test.rb
@@ -4,5 +4,33 @@ puts "WHADDAYA MEAN \"#{answer}\"?!? YOU\'RE FIRED"
 
 textwidth=50
 puts "Table of Content".center textwidth
-puts "Chapter 1: Numbers".ljust(textwidth)+"Page 1".rjust(textwidth)
-puts "Chapter 2: Letters".ljust textwidth + "Page 72".rjust textwidth
+puts "Chapter 1: Numbers".ljust(textwidth/2)+"Page 1".rjust(textwidth/2)
+puts "Chapter 2: Letters".ljust(textwidth/2) + "Page 72".rjust(textwidth/2)
+
+a=-4.abs
+puts a
+a=5**2
+puts a
+
+puts rand
+puts rand
+puts rand
+puts rand(1)
+puts rand(1)
+puts rand(100)
+puts rand(100)
+puts rand(5)
+puts rand(6)
+puts rand(100.0)
+
+srand 30
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts ""
+srand 30
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(100)
